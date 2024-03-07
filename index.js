05b1e7ebc0ae8ad6d9529e48f4d360154dbb2fae
Krii41 <134600504+Krii41@users.noreply.github.com>
2024-03-07 19:42:29 +0100

    Commit initial files

---
+++ index.js
@@ -0,0 +1,44 @@
+
+
+let resultH = document.getElementById("result-h");
+let result = 0;
+
+function add1h() {
+    result += 1;
+    resultH.innerText = result;
+}
+
+function add2h() {
+    result += 2;
+    resultH.innerText = result;
+}
+
+function add3h() {
+    result += 3;
+    resultH.innerText = result;
+}
+
+let resultG = document.getElementById("result-g");
+let results = 0;
+
+function add1g() {
+    results += 1;
+    resultG.innerText = results;
+}
+
+function add2g() {
+    results += 2;
+    resultG.innerText = results;
+}
+
+function add3g() {
+    results += 3;
+    resultG.innerText = results;
+}
+
+
+
+
+
+
+
